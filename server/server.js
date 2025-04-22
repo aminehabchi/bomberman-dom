af2d7c2fbb052f678686404e3fddbb2ab01f3ffd
aminehabchi <amine.habchi.01@gmail.com>
2025-04-22 23:34:04 +0100

    chat

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
-import http from "http"
+import http from "http";
 import routing from "./routes/Routes.js";
+import { Server } from "socket.io";
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  routing(req, res)
+  routing(req, res);
+});
+
+const io = new Server(server);
+
+// Listen for client connections
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+
+  // Handle messages from the client
+  socket.on("message", (msg) => {
+    console.log("Message from client:", msg);
+
+    socket.broadcast.emit("message", msg);
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
 server.listen(PORT, () => {
