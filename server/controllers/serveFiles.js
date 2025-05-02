0ed1cf58ef76d7f351cc93218991a1be82cbe6c5
mohammed fadil <fadil.ue2006@gmail.com>
2025-05-02 14:16:07 +0100

    Optimisation

--- server/controllers/serveFiles.js
+++ server/controllers/serveFiles.js
@@ -6,34 +6,12 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const webDir = path.join(__dirname, "../../web"); // Use path.join to ensure proper path formation
+const webDir = path.join(__dirname, "../../web");
 
 function serveFilesHandler(req, res) {
-  // Construct the file path
   const filePath = path.join(webDir, req.url);
-
-  // Read the requested file
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      // If there's an error reading the file (e.g., file not found), serve the index.html
-      if (req.url !== "/") {
-        const indexPath = path.join(webDir, "index.html"); // Path to index.html
-        fs.readFile(indexPath, (indexErr, indexContent) => {
-          if (indexErr) {
-            // If there's an error reading index.html, return a 500 server error
-            res.statusCode = 500;
-            res.setHeader("Content-Type", "text/plain");
-            res.end("Error loading index.html");
-          } else {
-            // Serve the index.html file if no other file found
-            const contentType = getContentType(indexPath);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", contentType);
-            res.end(indexContent);
-          }
-        });
-      } else {
-        // If the request is for the root ("/"), serve index.html
         const indexPath = path.join(webDir, "index.html");
         fs.readFile(indexPath, (indexErr, indexContent) => {
           if (indexErr) {
@@ -47,9 +25,7 @@ function serveFilesHandler(req, res) {
             res.end(indexContent);
           }
         });
-      }
     } else {
-      // If the requested file is found, serve it with the appropriate content type
       const contentType = getContentType(filePath);
       res.statusCode = 200;
       res.setHeader("Content-Type", contentType);
