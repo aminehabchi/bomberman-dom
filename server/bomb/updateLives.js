528265d6cdee62d23c8f213ea088cc9c32b517ef
mohammed fadil <fadil.ue2006@gmail.com>
2025-05-02 14:17:35 +0100

    Optimisation

--- server/bomb/updateLives.js
+++ server/bomb/updateLives.js
@@ -3,13 +3,9 @@ import { Rooms } from "../moduls/room.js";
 import { lose } from "../moduls/player.js";
 export function updateLife(playerNbr, roomUuid) {
   let player = Rooms[roomUuid].Players[playerNbr - 1];
-  console.log(player)
-  console.log(player)
   player.Lives--
-
   if (player.Lives == 0) {
     lose(player.Uuid)
   }
-
   io.to(roomUuid).emit("lives", { playerNbr: playerNbr, lives: player.Lives });
 }
